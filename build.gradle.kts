f75890236641c98825db259353e9a3e1d968a997
taratara10 <shuhei.knhs@gmail.com>
2024-06-16 22:19:54 +0900

    remove jvm dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,4 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
