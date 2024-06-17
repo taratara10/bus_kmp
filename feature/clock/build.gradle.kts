e679ce0160dcf462b92a3d11e80fa9e32c965c69
taratara10 <64339483+taratara10@users.noreply.github.com>
2024-06-17 22:10:33 +0900

    #3 :feature:clockの導入 (#6)
    * add feature:clock module
    
    * format

---
+++ feature/clock/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+//    alias(libs.plugins.androidLibrary)
+}
+
+
+kotlin {
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser {
+            commonWebpackConfig {
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static = (static ?: mutableListOf()).apply {
+                        // Serve sources to debug inside browser
+                        add(project.projectDir.path)
+                    }
+                }
+            }
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "Shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            commonMain.dependencies {
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                implementation(compose.ui)
+                implementation(compose.components.resources)
+                implementation(compose.components.uiToolingPreview)
+                implementation(projects.shared)
+            }
+        }
+    }
+}
---
+++ feature/clock/src/commonMain/kotlin/io/github/bus/ClockScreen.kt
@@ -0,0 +1,33 @@
+package io.github.bus
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ClockScreen() {
+    Scaffold {
+        Card(
+            backgroundColor = Color.Gray.copy(alpha = 0.3f),
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Column {
+                Text("After 12:00")
+                Text(
+                    text = "01:23",
+                    color = Color.Red,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                )
+            }
+        }
+    }
+}
