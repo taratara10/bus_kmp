d9d6a5118c1c4d5e8a0fe1731346c90fc5565314
taratara10 <shuhei.knhs@gmail.com>
2024-06-17 22:08:40 +0900

    format

--- feature/clock/src/commonMain/kotlin/io/github/bus/ClockScreen.kt
+++ feature/clock/src/commonMain/kotlin/io/github/bus/ClockScreen.kt
@@ -1,20 +1,26 @@
 package io.github.bus
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun ClockScreen() {
     Scaffold {
-        Card {
+        Card(
+            backgroundColor = Color.Gray.copy(alpha = 0.3f),
+            modifier = Modifier.padding(16.dp)
+        ) {
             Column {
-                Text("12:00発まで")
+                Text("After 12:00")
                 Text(
                     text = "01:23",
                     color = Color.Red,
