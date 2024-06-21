22374e73677a99d786b5e0710305b22786147561
taratara10 <shuhei.knhs@gmail.com>
2024-06-21 19:13:31 +0900

    impl dialog

--- core/model/src/commonMain/kotlin/io/github/kabos/StationName.kt
+++ core/model/src/commonMain/kotlin/io/github/kabos/StationName.kt
@@ -5,7 +5,7 @@ import kotlin.jvm.JvmInline
 @JvmInline
 value class StationName(val name: String) {
     companion object {
-        val takinoi = StationName("田喜野井")
-        val tsudanuma = StationName("津田沼_津08")
+        val takinoi = StationName("takinoi")
+        val tsudanuma = StationName("tsudanuma_tsu08")
     }
 }
