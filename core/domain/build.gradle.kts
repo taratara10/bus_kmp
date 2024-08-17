1c0a4e5d866f83cd31d10ddd6c9949b8dec1dee6
taratara10 <shuhei.knhs@gmail.com>
2024-08-17 17:08:16 +0900

    rm androidGradleLibraryPlugin

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -5,7 +5,6 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidGradleLibraryPlugin)
     id("buildlogic.android")
 }
 
