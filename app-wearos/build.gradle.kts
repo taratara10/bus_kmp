a6e2cdefc693b8f18c2743ac51bec08b915d70ea
taratara10 <shuhei.knhs@gmail.com>
2024-08-19 12:09:29 +0900

    create wearOS

---
+++ app-wearos/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    alias(libs.plugins.androidGradlePlugin)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.composeCompiler)
+}
+
+android {
+    namespace = "io.github.kabos.bus"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "io.github.kabos.bus"
+        minSdk = 31
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.wearos)
+    debugImplementation(libs.bundles.wearos.debug)
+}
